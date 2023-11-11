8c4d2244c97611c2efab9aad259d56c173ff6bfc
akozlova <anna.kozlova.ext@nokia.com>
2023-11-11 23:53:13 +0200

    pip install uvicorn

--- app/streamlit_run.py
+++ app/streamlit_run.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     subprocess.run(["pip", "install", "fastapi"])
     subprocess.run(["pip", "install", "torch==2.1.0"])
     subprocess.run(["pip", "install", "numpy==1.26.1"])
+    subprocess.run(["pip", "install", "uvicorn"])
     subprocess.run(["python", "-m", "app.main"])
