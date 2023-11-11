8100abf87beb69bef825ee1f7b708f8265c3913c
cewinharhar <kevin.yar@outlook.com>
2023-11-11 10:42:45 +0100

    new try

---
+++ jsEx/charts.js
@@ -0,0 +1,39 @@
+// Import Chart.js if you're using modules, otherwise it's already globally available
+// import Chart from 'chart.js';
+
+export function renderCharts(data) {
+    // Extract data for each measurement
+    const heartRateData = data.map(item => ({ x: item.timestep, y: item["heart rate"] }));
+    const temperatureData = data.map(item => ({ x: item.timestep, y: item.temperature }));
+    const rhbData = data.map(item => ({ x: item.timestep, y: item.rhb }));
+
+    // Render charts
+    renderChart('heart-rate-chart', heartRateData, 'Heart Rate');
+    renderChart('temperature-chart', temperatureData, 'Temperature');
+    renderChart('rhb-chart', rhbData, 'RHB');
+}
+
+function renderChart(canvasId, data, label) {
+    const ctx = document.getElementById(canvasId).getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: label,
+                data: data,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    type: 'linear',
+                    position: 'bottom'
+                }
+            },
+            maintainAspectRatio: false, 
+        }
+    });
+}
--- jsEx/script20.js
+++ jsEx/script20.js
@@ -1,68 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Initial data loading and chart rendering
-    loadDataAndRenderCharts();
+// Modularize the chart rendering logic
+import { renderCharts } from './charts.js';
 
-    // Event listener for the button
-    document.getElementById('change-data-button').addEventListener('click', function() {
-        loadDataAndRenderCharts();
+document.addEventListener('DOMContentLoaded', async function() {
+    await loadDataAndRenderCharts();
+
+    const changeDataButton = document.getElementById('change-data-button');
+    changeDataButton.addEventListener('click', async function() {
+        await loadDataAndRenderCharts();
         fetchTextAndUpdate();
     });
 });
 
-function loadDataAndRenderCharts() {
-    fetch('data.json') // Adjust this to your JSON file's path
-        .then(response => response.json())
-        .then(data => {
-            renderCharts(data);
-        })
-        .catch(error => console.error('Error:', error));
+async function loadDataAndRenderCharts() {
+    // try {
+        const response = await fetch('data.json');
+        const allData = await response.json();
+        
+        // Filter data for scenario 1
+        const scenario1Data = allData.filter(item => item.scenario === 1);
+
+        renderCharts(scenario1Data);
+    // } catch (error) {
+    //     console.error('Error:', error);
+    //     // Implement more robust error handling here
+    // }
 }
 
-function renderCharts(data) {
-    // Extract data for each measurement
-    let heartRateData = data.map(item => ({ x: item.timestep, y: item["heart rate"] }));
-    let temperatureData = data.map(item => ({ x: item.timestep, y: item.temperature }));
-    let rhbData = data.map(item => ({ x: item.timestep, y: item.rhb }));
-
-    // Render chart for Heart Rate
-    renderChart('heart-rate-chart', heartRateData, 'Heart Rate');
-
-    // Render chart for Temperature
-    renderChart('temperature-chart', temperatureData, 'Temperature');
-
-    // Render chart for RHB
-    renderChart('rhb-chart', rhbData, 'RHB');
-}
-
-function renderChart(canvasId, data, label) {
-    const ctx = document.getElementById(canvasId).getContext('2d');
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            datasets: [{
-                label: label,
-                data: data,
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-                tension: 0.1
-            }]
-        },
-        options: {
-            scales: {
-                x: {
-                    type: 'linear',
-                    position: 'bottom'
-                }
-            }
-        }
-    });
-}
-
-function fetchTextAndUpdate() {
-    fetch('api_endpoint_here') // Replace with your actual API endpoint
-        .then(response => response.text())
-        .then(text => {
-            document.getElementById('text-block').innerText = text;
-        })
-        .catch(error => console.error('Error:', error));
+async function fetchTextAndUpdate() {
+    try {
+        const response = await fetch('api_endpoint_here');
+        const text = await response.text();
+        document.getElementById('text-block').innerText = text;
+    } catch (error) {
+        console.error('Error:', error);
+        // Handle error appropriately
+    }
 }
