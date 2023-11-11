c75a2fd7884c22f4c069fb4a7e09c3a8123d5014
akozlova <anna.kozlova.ext@nokia.com>
2023-11-11 23:31:28 +0200

    pip install torch

--- app/streamlit_run.py
+++ app/streamlit_run.py
@@ -3,4 +3,5 @@ import subprocess
 # Run the Streamlit app
 if __name__ == "__main__":
     subprocess.run(["pip", "install", "fastapi"])
+    subprocess.run(["pip", "install", "torch==2.1.0"])
     subprocess.run(["python", "-m", "app.main"])
