bebae9e1f7faff33a9a366231ef80d7e107d6f54
akozlova <anna.kozlova.ext@nokia.com>
2023-11-11 23:37:53 +0200

    pip install numpy

--- app/streamlit_run.py
+++ app/streamlit_run.py
@@ -4,4 +4,5 @@ import subprocess
 if __name__ == "__main__":
     subprocess.run(["pip", "install", "fastapi"])
     subprocess.run(["pip", "install", "torch==2.1.0"])
+    subprocess.run(["pip", "install", "numpy==1.26.1"])
     subprocess.run(["python", "-m", "app.main"])
